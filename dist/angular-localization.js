c248ba6d1e7a5dd6ab276b0047a0259aef11ee6d
zolotovvv <zolotovvv@outlook.com>
2016-09-08 14:43:30 +0300

    Revert "Set deleteOnExpire type"
    This reverts commit 232d239380781264485b1485acc0ed5038f3b1b2.

--- dist/angular-localization.js
+++ dist/angular-localization.js
@@ -33,7 +33,7 @@ angular.module('ngLocalize')
         CacheFactory('localeCache', {
             maxAge: 15 * 60 * 1000, // Items added to this cache expire after 15 minutes
             cacheFlushInterval: 60 * 60 * 1000, // This cache will clear itself every hour
-            deleteOnExpire: 'passive', // Items will be deleted from this cache when they expire
+            deleteOnExpire: 'aggressive', // Items will be deleted from this cache when they expire
             storageMode: 'localStorage',
             storagePrefix: 'bt'
         });
